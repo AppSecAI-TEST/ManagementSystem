b281aa7faf001cf18e8d066cef55e153c1a6e3a1
ITBANK403-15 <ITBANK403-15@ITBANK403-15-PC>
2017-08-10 14:38:07 +0900

    //메일

--- ManagementSystem/src/Email.java
+++ ManagementSystem/src/Email.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Dimension;
+import java.awt.FileDialog;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
@@ -8,6 +9,8 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -23,7 +26,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-public class Email extends JPanel {
+public class Email extends JPanel{
 	Email self = this;
 	CardLayout ca;
 	private JPanel panel = new JPanel();
@@ -38,17 +41,20 @@ public class Email extends JPanel {
 	private JButton addressbook = new JButton(new ImageIcon("C:/Users/ITBANK403-15/Pictures/b.jpg"));
 	private JLabel Reference = new JLabel("참조  ");
 	private JTextField reference = new JTextField(59);
-	private JLabel Attachedfile = new JLabel("첨부파일");
-	private JTextField attachedfile = new JTextField(59);
+	private JLabel Attached = new JLabel("첨부파일");
+	private JButton AttachedFile = new JButton(new ImageIcon("C:/Users/ITBANK403-15/Pictures/c.jpg"));
+	private JTextField Attachedfile = new JTextField(59);
+	private FileDialog attachedfile;
 	private JLabel Text = new JLabel("내용  ");
-	private JTextArea text = new JTextArea(18, 59);
+	private JTextArea text = new JTextArea(17, 59);
 	private JScrollPane scroll = new JScrollPane(text);
 	private JButton send = new JButton("전송");
+	
 	
 	public void compInit() {
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		c.insets = new Insets(10, 25, 0, 5);
+		c.insets = new Insets(10, 5, 0, 5);
 		c.gridy = 1;c.gridx = 1;
 		this.add(ReceiveEmail, c);
 		c.gridy = 1;c.gridx = 2;
@@ -64,9 +70,11 @@ public class Email extends JPanel {
 		c.gridy = 3;c.gridx = 2;
 		this.add(reference, c);
 		c.gridy = 4;c.gridx = 1;
-		this.add(Attachedfile, c);
+		this.add(Attached, c);
 		c.gridy = 4;c.gridx = 2;
-		this.add(attachedfile, c);
+		this.add(Attachedfile, c);
+		c.gridy = 4;c.gridx = 3;
+		this.add(AttachedFile, c);
 		c.gridy = 5;c.gridx = 1;
 		this.add(Text, c);
 		c.gridy = 6;c.gridx = 2;
@@ -74,11 +82,13 @@ public class Email extends JPanel {
 		c.gridy = 7;c.gridx = 3;
 		this.add(send, c);
 	}
+	public void eventInit(){
+	}
 
 	public Email() {
 		this.setSize(800, 700);
 		this.compInit();
-		// this.setResizable(false);
+		this.eventInit();
 		this.setVisible(true);
 	}
 
